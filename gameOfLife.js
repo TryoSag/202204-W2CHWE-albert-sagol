3479d97094ea64b419a3c75d8be425a376450185
Albert Sagol <albert.sagol@gmail.com>
2022-04-17 23:17:38 +0200

    added Js functions to create and clean table in the Html

--- gameOfLife.js
+++ gameOfLife.js
@@ -31,6 +31,7 @@ const aliveAround = (axisX, axisY, limitAxisX, limitAxisY, screenArray) => {
   }
   return neighborAlive;
 };
+
 const cellState = (actualX, actualY, limitX, limitY, screenArray) => {
   const cellAround = aliveAround(actualX, actualY, limitX, limitY, screenArray);
 
@@ -42,4 +43,55 @@ const cellState = (actualX, actualY, limitX, limitY, screenArray) => {
   }
   return true;
 };
-cellState();
+
+const tableCreator = (sizeX, sizeY, tableArray) => {
+  const gameTable = document.getElementById("game-table");
+
+  for (let x = 0; x < sizeX; x++) {
+    const newColumn = document.createElement("div");
+    for (let y = 0; y < sizeY; y++) {
+      const newSquare = document.createElement("div");
+      if (tableArray[x][y]) {
+        newSquare.classList.add("itsAlive");
+      }
+      newColumn.appendChild(newSquare);
+    }
+    gameTable.appendChild(newColumn);
+  }
+};
+
+const cleanTable = () => {
+  document.getElementById("game-table").innerHTML = "";
+};
+
+const randomArray = (sizeX, sizeY) => {
+  const randomStart = [];
+
+  for (let x = 0; x < sizeX; x++) {
+    for (let y = 0; y < sizeY; y++) {
+      randomStart[x][y] = Math.random() > 0.66;
+    }
+  }
+  return randomStart;
+};
+
+const gameOfLife = () => {
+  const nextTableArray = [];
+  const sizeX = 30;
+  const sizeY = 20;
+  let gameArray = randomArray(sizeX, sizeY);
+
+  while (true) {
+    tableCreator(sizeX, sizeY, gameArray);
+
+    for (let x = 0; x < sizeX; x++) {
+      for (let y = 0; y < sizeY; y++) {
+        nextTableArray[x][y] = cellState(x, y, sizeX, sizeY, gameArray);
+      }
+    }
+    gameArray = nextTableArray;
+    cleanTable();
+  }
+};
+
+gameOfLife();
